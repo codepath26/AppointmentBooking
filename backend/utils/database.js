8cd489bec290b6c7172cd843e6bccc918903124e
PARTH THAKOR <129299328+codepath26@users.noreply.github.com>
2023-09-05 15:51:06 +0530

    Update database.js

--- backend/utils/database.js
+++ backend/utils/database.js
@@ -1,6 +1,7 @@
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(process.env.DATABASE_NAME,process.env.DATABASE_USER,process.env.DATABASE_PASS,{
-  host : 'localhost',
+ host: '127.0.0.1', // or 'localhost'
+  port: 3306,
   dialect : 'mysql' ,
 })
 
